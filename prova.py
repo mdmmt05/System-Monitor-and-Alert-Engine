23254e42a7e7800a472ed846c617bd3d143c9adf
Michele Dimastromatteo <100855068+mdmmt05@users.noreply.github.com>
2026-02-23 09:24:49 +0100

    Delete prova.py

--- prova.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-def get_current_processes():
-    processes = []
-    for proc in psutil.process_iter(['pid', 'name']):
-        processes.append(proc.info)
-    return processes
-
-print(get_current_processes())
